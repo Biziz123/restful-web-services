15056950032eb0c8190149fda921c8dd72619999
Gaurav <gauravsinghkhon@gmail.com>
2024-02-02 11:11:18 +0530

    Intialized

---
+++ src/main/java/com/socialmedia/rest/webservices/restfulwebservices/user/UserDaoService.java
@@ -0,0 +1,26 @@
+package com.socialmedia.rest.webservices.restfulwebservices.user;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDaoService {
+    
+	
+	private static List<User> users= new ArrayList<>();
+	
+	static {
+		users.add(new User(1,"Adam",LocalDate.now().minusYears(30)));
+		users.add(new User(2,"Amit",LocalDate.now().minusYears(20)));
+		users.add(new User(3,"Sourav",LocalDate.now().minusYears(15)));
+	}
+	
+	public List<User> findAll(){
+		return users;
+	}
+	
+	
+			}
